9cce37d41e6bfc82b6c9a1304eaddc25d23328a3
Trevor <trevor@twgreengold.com>
2021-03-03 15:45:38 +0800

    add migration and model

---
+++ database/migrations/2021_03_03_115613_create_articles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->index();
+            $table->text('content');
+            $table->string('author', 50)->index();
+            $table->date('created_date')->index();
+            $table->date('updated_date')->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}

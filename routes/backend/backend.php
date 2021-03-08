d9c46460e26f54a7e3cadf11c2b873c221e48dc9
Trevor <trevor@twgreengold.com>
2021-03-08 17:42:59 +0800

    update route

--- routes/backend/backend.php
+++ routes/backend/backend.php
@@ -1,4 +1,17 @@
 <?php
 
+// 登入頁
+Route::get('login', 'backend\LoginController@show')->name('backend.login.show');
+// 登入
+Route::post('login', 'backend\LoginController@login')->name('backend.login.login');
+// 登出
+Route::get('logout', 'backend\LoginController@logout')->name('backend.login.logout');
+
+Route::middleware(['auth:users'])->group(function () {
+
+    return 'Hello';
+
+});
+
 // test
 require 'test.php';

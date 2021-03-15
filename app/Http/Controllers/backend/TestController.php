9fccdd1dc79671808e6a764634e27e876c39fe5f
Trevor <trevor@twgreengold.com>
2021-03-15 10:57:11 +0800

    添加 測試 test Controller and blade

---
+++ app/Http/Controllers/backend/TestController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\backend;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UserLoginRequest;
+use App\Models\User;
+use Debugbar;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Libraries\Helper\HelperSession;
+use Libraries\Helper\HelperValidator;
+use Libraries\Helper\HelpImageUploader;
+
+class TestController extends Controller
+{
+    public function testPost(Request $request)
+    {
+        dump($request->file('fileName'));
+//        dump($request->file('fileNames'));
+
+        HelpImageUploader::uploadFileAtTable($request->file('fileName'), 'users', 'name', User::find(1));
+
+        dd($request);
+    }
+}
---
+++ resources/views/backend/test.blade.php
@@ -0,0 +1,24 @@
+<?php
+use Libraries\Helper\HelpImageUploader;
+
+$HelpImageUploader = new HelpImageUploader();
+$HelpImageUploader->setStorageDisk('public');
+?>
+<!-- Scripts -->
+<script src="{{asset('js/app.js')}}" defer></script>
+<!-- Styles -->
+<link href="{{asset('css/app.css')}}" rel="stylesheet">
+
+<form action="{{route('backend.test.testPost')}}" enctype="multipart/form-data" method="post">
+    @csrf
+    <div class="m-2 p-2">
+        <span class="text-primary"> 上傳一個 : </span>
+        <input type="file" id="fileId" name="fileName" value="">
+    </div>
+    <div class="m-2 p-2">
+        <span class="text-primary"> 上傳多個 : </span>
+        <input type="file" id="fileIds" name="fileNames[]" value="" multiple>
+    </div>
+
+    <button type="submit" class="w-25 btn btn-primary btn-block">送出</button>
+</form>

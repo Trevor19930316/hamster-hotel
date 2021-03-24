36625301e7fa713579ab3626eb05f08cdde25360
Tracy <lai831009@gmail.com>
2021-03-24 17:35:11 +0800

    後台 | 更新 component input-group markdown

--- resources/views/backend/components/base/input-group.blade.php
+++ resources/views/backend/components/base/input-group.blade.php
@@ -28,6 +28,25 @@
                             @endcomponent
                         @endslot
                     @endcomponent
+                    @component('backend.template.components.base.row.markdown')
+                        @slot('content')
+                            @markdown
+                            @verbatim
+                                @component('backend.template.components.base.input-group')
+                                    @slot('prependCount',1)
+                                    @slot('prependType1','text')
+                                    @slot('prependContent1','user')
+                                    @slot('contentDiv')
+                                        text
+                                    @endslot
+                                    @slot('appendCount',1)
+                                    @slot('appendType1','text')
+                                    @slot('appendContent1','user')
+                                @endcomponent
+                            @endverbatim
+                            @endmarkdown
+                        @endslot
+                    @endcomponent
                     @component('backend.template.components.base.row.form-row')
                         @slot('label','內容 Input')
                         @slot('content')
@@ -65,6 +84,108 @@
                                     <button class="btn btn-secondary" type="button">btn</button>
                                 @endslot
                             @endcomponent
+                        @endslot
+                    @endcomponent
+                @endslot
+            @endcomponent
+        </div>
+        <div class="col-sm-6">
+            @component('backend.template.components.base.cards.card')
+                @slot('cardTitle')
+                    input group
+                @endslot
+                @slot('cardContent')
+                    @component('backend.template.components.base.row.form-row')
+                        @slot('label','內容 Input')
+                        @slot('content')
+                            @component('backend.template.components.base.input-group')
+                                @slot('prependCount',1)
+                                @slot('prependType1','text')
+                                @slot('prependContent1','@')
+                                @slot('content')
+                                    <?php
+                                    $EInput->setName('text');
+                                    $EInput->setType('text');
+                                    $EInput->show();
+                                    ?>
+                                @endslot
+                            @endcomponent
+                        @endslot
+                    @endcomponent
+                    @component('backend.template.components.base.row.markdown')
+                        @slot('content')
+                            @markdown
+                            @verbatim
+                                @component('backend.template.components.base.input-group')
+                                    @slot('prependCount',1)
+                                    @slot('prependType1','text')
+                                    @slot('prependContent1','@')
+                                    @slot('content')
+                                        &lt;?php
+                                        $EInput->setName('text');
+                                        $EInput->setType('text');
+                                        $EInput->show();
+                                        ?>
+                                    @endslot
+                                @endcomponent
+                            @endverbatim
+                            @endmarkdown
+                        @endslot
+                    @endcomponent
+                @endslot
+            @endcomponent
+        </div>
+        <div class="col-sm-6">
+            @component('backend.template.components.base.cards.card')
+                @slot('cardTitle')
+                    input group
+                @endslot
+                @slot('cardContent')
+                    @component('backend.template.components.base.row.form-row')
+                        @slot('label','前後 button')
+                        @slot('content')
+                            @component('backend.template.components.base.input-group')
+                                @slot('prependCount',1)
+                                @slot('prependContent1')
+                                    <button class="btn btn-secondary" type="button">btn</button>
+                                @endslot
+                                @slot('content')
+                                    <?php
+                                    $EInput->setName('text');
+                                    $EInput->setType('text');
+                                    $EInput->show();
+                                    ?>
+                                @endslot
+                                @slot('appendCount',1)
+                                @slot('appendContent1')
+                                    <button class="btn btn-secondary" type="button">btn</button>
+                                @endslot
+                            @endcomponent
+                        @endslot
+                    @endcomponent
+                    @component('backend.template.components.base.row.markdown')
+                        @slot('content')
+                            @markdown
+                            @verbatim
+                                @component('backend.template.components.base.input-group')
+                                    @slot('prependCount',1)
+                                    @slot('prependContent1')
+                                        <button class="btn btn-secondary" type="button">btn</button>
+                                    @endslot
+                                    @slot('content')
+                                        &lt;?php
+                                        $EInput->setName('text');
+                                        $EInput->setType('text');
+                                        $EInput->show();
+                                        ?>
+                                    @endslot
+                                    @slot('appendCount',1)
+                                    @slot('appendContent1')
+                                        <button class="btn btn-secondary" type="button">btn</button>
+                                    @endslot
+                                @endcomponent
+                            @endverbatim
+                            @endmarkdown
                         @endslot
                     @endcomponent
                 @endslot

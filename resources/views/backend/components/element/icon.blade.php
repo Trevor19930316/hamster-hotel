c78e8b188cb37db985d7f7977450a191d63a0ab9
Trevor <trevor@twgreengold.com>
2021-03-24 14:06:23 +0800

    Update Icon

--- resources/views/backend/components/element/icon.blade.php
+++ resources/views/backend/components/element/icon.blade.php
@@ -15,7 +15,7 @@
                         @slot('label','上一頁')
                         @slot('content')
                             <?php
-                            $EIcon = new \Libraries\element\EIcon();
+                            use Illuminate\Support\Facades\Blade;$EIcon = new \Libraries\element\EIcon();
                             $EIcon->setIcon('arrow-left');
                             $EIcon->setTitle('上一頁');
                             $EIcon->show();
@@ -31,6 +31,32 @@
                             $EIcon->setTitle('下一頁');
                             $EIcon->show();
                             ?>
+                        @endslot
+                    @endcomponent
+                @endslot
+            @endcomponent
+            @component('backend.template.components.base.cards.card')
+                @slot('cardTitle')
+                    Icon
+                @endslot
+                @slot('cardContent')
+                    @component('backend.template.components.base.row.form-row')
+                        @slot('label','code')
+                        @slot('content')
+                            <?php
+                            //compileString
+                            $code = '$EIcon = new \Libraries\element\EIcon();
+                            $EIcon->setIcon(\'arrow-right\');';
+                            //$code .= '$EIcon->setIcon(\'arrow-right\');';
+                            //$code .= '$EIcon->setTitle(\'下一頁\');';
+                            //$code .= '$EIcon->show();';
+                            $code = trim(Blade::compileString($code));
+                            //$showCode = Blade::compileString($code);
+                            //$showCode = htmlspecialchars($code);
+                            ?>
+                            <pre class="ds-border p-3 rounded bg-light cursor-pointer">
+                                {!! $code !!}
+                            </pre>
                         @endslot
                     @endcomponent
                 @endslot

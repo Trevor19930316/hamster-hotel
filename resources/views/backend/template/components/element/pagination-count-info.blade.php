fc478720096d6e36aacc9b66e8b54e7c4a123e28
Trevor <trevor@twgreengold.com>
2021-03-30 17:58:28 +0800

    Add EPaginationCountInfo Part.02

--- resources/views/backend/template/components/element/pagination-count-info.blade.php
+++ resources/views/backend/template/components/element/pagination-count-info.blade.php
@@ -4,9 +4,9 @@ $pageRowsCount = $pageRowsCount ?? null;
 $totalPageCount = $totalPageCount ?? null;
 ?>
 <div class="text-center text-md-left d-flex">
-    <span class="p-2">{{__('pagination.page_data_count', ['count' => $pageRowsCount])}}</span>
-    <span class="p-2">|</span>
-    <span class="p-2">{{__('pagination.total_page', ['count' => $totalPageCount])}}</span>
-    <span class="p-2">|</span>
-    <span class="p-2">{{__('pagination.total_data_count', ['count' => $totalRowsCount])}}</span>
+    <span class="p-1">{{__('pagination.page_data_count', ['count' => $pageRowsCount])}}</span>
+    <span class="p-1">|</span>
+    <span class="p-1">{{__('pagination.total_page', ['count' => $totalPageCount])}}</span>
+    <span class="p-1">|</span>
+    <span class="p-1">{{__('pagination.total_data_count', ['count' => $totalRowsCount])}}</span>
 </div>

39482f3e4e557d79175afd817b89c094392eebce
Trevor <trevor@twgreengold.com>
2021-03-30 15:19:15 +0800

    Add EPagination Part.02

--- resources/views/backend/template/components/element/pagination.blade.php
+++ resources/views/backend/template/components/element/pagination.blade.php
@@ -6,13 +6,13 @@ $previousPage = $previousPage ?? 1;
 $nextPage = $nextPage ?? 1;
 ?>
 <div class="pagination d-flex align-items-center">
-    <span class="page-item ">
-        <span class="page-link text-info" onclick="paginationChangePage($(this.form), 1)">First</span>
-    </span>
-    <span class="page-item">
+    <bu class="page-item cursor-pointer">
+        <span class="page-link text-info " onclick="paginationChangePage($(this.form), 1)">First</span>
+    </bu>
+    <span class="page-item cursor-pointer">
         <span class="page-link text-info" onclick="paginationChangePage($(this.form), {{$previousPage}})">Prev</span>
     </span>
-    <span class="page-item">
+    <span class="page-item cursor-pointer">
         <?php
         $EInput = new EInput();
         $EInput->setClass('w-auto text-center');
@@ -23,10 +23,10 @@ $nextPage = $nextPage ?? 1;
         $EInput->show();
         ?>
     </span>
-    <span class="page-item">
+    <span class="page-item cursor-pointer">
         <span class="page-link text-info" onclick="paginationChangePage($(this.form), {{$nextPage}})">Next</span>
     </span>
-    <span class="page-item">
+    <span class="page-item cursor-pointer">
         <span class="page-link text-info" onclick="paginationChangePage($(this.form), {{$lastPage}})">Last</span>
     </span>
 </div>
